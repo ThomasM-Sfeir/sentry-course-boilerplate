79bfccf8ba49e05b2af266aeed7a2cff74ff0de4
ThomasM-Sfeir <moreau.t@sfeir.com>
2024-02-13 15:33:02 +0100

    Add releases config + Fix Error

--- src/main.js
+++ src/main.js
@@ -2,7 +2,29 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import "@/assets/main.pcss"
+import * as Sentry from "@sentry/vue";
 
-createApp(App)
-.use(router)
-.mount('#app')
+const app = createApp(App)
+
+Sentry.init({
+    app,
+    dsn: "https://a7425bea0c1e50f7ccfa30f4b06fc8dc@o4506738748948480.ingest.sentry.io/4506738802688000",
+    logErrors: true,
+    release: __SENTRY_RELEASE__,
+    integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.replayIntegration({
+            maskAllText: false,
+            blockAllMedia: false,
+        }),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+app.use(router).mount('#app')
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,12 @@
+<script setup>
+const checkout = () => {
+  alert("Checkout out items")
+}
+</script>
+
 <template>
   <div>
     <h1 class="text-4xl">Home</h1>
+    <button @click="checkout">Checkout</button>
   </div>
 </template>
